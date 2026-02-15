7e02efc32017aec3c4e108635314b31fb7e29544
kim beom seok <qjatjr1285@naver.com>
2026-02-15 17:27:30 +0900

    20260215 - 착용 아이템 정보 그리드를 메이플랜드 내의 장비창과 비슷하도록 수정

--- MapleBuildLab/src/components/EquipmentGrid.tsx
+++ MapleBuildLab/src/components/EquipmentGrid.tsx
@@ -21,20 +21,54 @@ export default function EquipmentGrid() {
     "PET",
   ];
 
+    // 스샷처럼 "정해진 위치"에 슬롯을 배치할 거라서, map 반복 대신
+  // 각 슬롯을 고정된 grid-area로 렌더링합니다.
   return (
     <section className="panel">
       <h2 className="panelTitle">착용 아이템 정보</h2>
 
       <div className="equipWindow">
         <div className="equipHeader">EQUIPMENT INVENTORY</div>
-        <div className="equipGrid">
-          {slots.map((s, idx) => (
-            <div key={`${s}-${idx}`} className="equipSlot">
-              {s}
-            </div>
-          ))}
+
+        <div className="equipGrid equipGridAreas">
+          {/* row1 */}
+          <div className="equipSlot area-cap">CAP</div>
+
+          {/* row2 */}
+          <div className="equipSlot area-medal">MEDAL</div>
+          <div className="equipSlot area-forehead">FOREHEAD</div>
+          <div className="equipSlot area-ring1">RING</div>
+          <div className="equipSlot area-ring2">RING</div>
+
+          {/* row3 */}
+          <div className="equipSlot area-eye">EYE ACC</div>
+          <div className="equipSlot area-ear">EAR ACC</div>
+
+          {/* row4 */}
+          <div className="equipSlot area-mantle">MANTLE</div>
+          <div className="equipSlot area-clothes">CLOTHES</div>
+          <div className="equipSlot area-pendant">PENDANT</div>
+          <div className="equipSlot area-weapon">WEAPON</div>
+          <div className="equipSlot area-shield">SHIELD</div>
+
+          {/* row5 */}
+          <div className="equipSlot area-gloves">GLOVES</div>
+          <div className="equipSlot area-pants">PANTS</div>
+          <div className="equipSlot area-belt">BELT</div>
+          <div className="equipSlot area-ring3">RING</div>
+          <div className="equipSlot area-ring4">RING</div>
+
+          {/* row6 */}
+          <div className="equipSlot area-shoes">SHOES</div>
+
+          {/* row7 */}
+          <div className="equipSlot area-taming">TAMING MOB</div>
+          <div className="equipSlot area-saddle">SADDLE</div>
+          <div className="equipSlot area-mob">MOB EQUIP</div>
+          <div className="equipSlot area-petacc">PET ACC</div>
         </div>
       </div>
     </section>
   );
+
 }
